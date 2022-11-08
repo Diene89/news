6fb9d1272a2ad060fb74d78affe9d462709a8594
Diene89 <diene_89@hotmail.com>
2022-11-08 16:21:04 -0300

    feat: created function that is responsible for making the HTTP request to the site and getting the HTML content

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,19 @@
-# Requisito 1
+import requests
+import time
+
+
 def fetch(url):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(
+            url, headers={"user-agent": "Fake user-agent"}, timeout=3
+        )
+        time.sleep(1)
+        if response.status_code == 200:
+            return response.text
+        else:
+            return None
+    except requests.ReadTimeout:
+        return None
 
 
 # Requisito 2
